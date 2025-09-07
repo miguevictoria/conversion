b00930f1b6bcfd58899ab3fc3b9648b73be27e89
Miguel Victoria <51333774+miguevictoria@users.noreply.github.com>
2025-09-07 14:51:44 -0600

    Mejor codificacion
    Se agregaron y eliminaron algunos elementos. Ademas ya tiene un Return en cada funcion

--- conversion.py
+++ conversion.py
@@ -1,12 +1,12 @@
 def liters_100km_to_miles_gallon(liters):
-    (100/1.609344)/(liters/3.785411784)
+    return (100 / 1.609344) / (liters / 3.785411784)
 
-def miles_gallon_to_liters_100km(miles):
-    (100*3.785411784)/(miles*1.609344)
+def miles_gallon_to_liters_100km(mpg):
+    return (100 * 3.785411784) / (mpg * 1.609344)
 
 print(liters_100km_to_miles_gallon(3.9))
 print(liters_100km_to_miles_gallon(7.5))
-print(liters_100km_to_miles_gallon(10.))
+print(liters_100km_to_miles_gallon(10.0))
 print(miles_gallon_to_liters_100km(60.3))
 print(miles_gallon_to_liters_100km(31.4))
 print(miles_gallon_to_liters_100km(23.5))
